ca25141ca01982303bc0d46be0eb2d4fa0c04a9e
Mariana <blue.atabey@gmail.com>
2018-03-15 14:25:39 -0400

    Add Twitter Feed

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -63,7 +63,16 @@ get_header(); ?>
 					<?php wp_reset_query(); ?>		
 				</div> <!-- blog-post -->
 			
-		</section>	
+		</section> <!-- .recent-posts -->
+
+		<section class="recent-tweets">
+			<h4>Recent Tweet</h4>
+			<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+			<div id="secondary" class="widget-area" role="complementary">
+				<?php dynamic_sidebar( 'sidebar-2' ); ?>
+			</div>
+			<?php endif; ?>		
+		</section> <!-- .recent-tweets	 -->
 
 
 <?php get_footer(); ?>
--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -14,7 +14,9 @@
 function accelerate_child_scripts(){
 	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
-  wp_enqueue_style('font-awesome','//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');}
+  wp_enqueue_style('font-awesome','//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('accelerate-child-google-fonts', '//fonts.googleapis.com/css?family=Londrina+Solid:400,900');
+}
 
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
 
@@ -59,3 +61,18 @@ add_action( 'init', 'create_custom_post_types' );
    } 
 
  add_action( 'widgets_init', 'widget_area_404' );  
+
+  function accelerate_theme_child_widget_init() {
+    register_sidebar( array (
+        'name' => __( 'Homepage sidebar', 'accelerate-theme-child'),
+        'id' => 'sidebar-2',
+        'description' => __( 'Appears on the static front page template', 'accelerate-theme-child'),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+      ));
+  }
+  
+  
+  add_action( 'widgets_init', 'accelerate_theme_child_widget_init' );  
